ea944c834aa4a65db36b41979b9006d3aebe3965
usuario <170405410+mgrl39@users.noreply.github.com>
2025-04-07 17:39:40 +0200

    add Seeder for database,add content in the models, removed route explorer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,1 +1,17 @@
 <?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $this->call(
+            [
+            MovieSeeder::class,
+            ]
+        );
+    }
+}
