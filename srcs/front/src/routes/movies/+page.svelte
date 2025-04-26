965c93144c11e677f39f3dcf4eee51d1f56a45b1
usuario <170405410+mgrl39@users.noreply.github.com>
2025-04-27 01:54:55 +0200

    modified title

--- srcs/front/src/routes/movies/+page.svelte
+++ srcs/front/src/routes/movies/+page.svelte
@@ -50,7 +50,11 @@
 </script>
 
 <div class="container mt-4">
-  <h1 class="mb-4">Películas</h1>
+  <div class="row">
+    <div class="col-12">
+      <h1 class="section-title mb-4">Peliculas</h1>
+    </div>
+  </div>
   
   {#if loading}
     <div class="d-flex justify-content-center my-5">
