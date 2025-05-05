516e50ff1ac6a6ec645fa10a2ed5b57f35731828
usuario <170405410+mgrl39@users.noreply.github.com>
2025-05-05 03:08:41 +0200

    add bookings and notifications pages (empty)

---
+++ srcs/front/src/routes/bookings/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { API_URL } from '$lib/config';
+  import { page } from '$app/stores';
+  import { SkeletonText, DataTable, Toolbar, ToolbarContent, ToolbarSearch, Button, Tag, Pagination } from 'carbon-components-svelte';
+</script>
+NO HAY NADA AUN
